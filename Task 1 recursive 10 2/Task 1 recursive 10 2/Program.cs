b2ac842b599ad90440641340f74c98368950a876
Metabolic-syndrome <pv.bruk@gmail.com>
2018-10-30 15:09:00 +0300

    1

---
+++ Task 1 recursive 10 2/Task 1 recursive 10 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_recursive_10_2
+{
+    class Program
+
+     // Реализовать функцию перевода из 10 системы в двоичную используя рекурсию.
+    {
+        static string a(int i)
+        {
+            return (i == 0) ? "0" : b(i);
+        }
+
+        static string b(int i)
+        {
+            return (i > 0) ? b(i / 2) + (i % 2).ToString() : "";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write(a(8));
+            Console.ReadKey();
+        }
+    }
+}
