c791bd2dc40d10871bbf610b987ccb5919c33d9e
Metabolic-syndrome <pv.bruk@gmail.com>
2018-11-08 14:11:40 +0300

    Lesson 5

---
+++ Queue/Task 6/Task 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+
+    // Реализовать очередь.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> q = new Queue<string>(5);
+            q.Enqueue("1");
+            q.Enqueue("2");
+            q.Enqueue("3");
+            q.Enqueue("4");
+            Console.WriteLine("Сделано: " + q.Dequeue());
+            Console.WriteLine("Сделано: " + q.Dequeue());
+            Console.WriteLine("Всего осталось: " + q.Count.ToString());
+            Console.Read();
+        }
+    }
+}
---
+++ Queue/Task 6/Task 6/Queue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+   public class Queue<T>
+    {
+        private int _Front = -1;
+        private int _Rear = -1;
+        private int _Count = 0;
+        private readonly int _Size;
+        private readonly T[] _Array;
+        public Queue(int Size)
+        {
+            this._Size = Size;
+            this._Array = new T[Size];
+        }
+
+        public bool IsFull()
+        {
+            return _Rear == _Size - 1;
+        }
+
+        public bool IsEmpty()
+        {
+            return _Count == 0;
+        }
+
+        public void Enqueue(T Item)
+        {
+            if (this.IsFull())
+                throw new Exception("Очередь полностью заполнена.");
+
+            _Array[++_Rear] = Item;
+            _Count++;
+        }
+
+        public T Dequeue()
+        {
+            if (this.IsEmpty())
+                throw new Exception("Очередь не заполнена.");
+
+            T value = _Array[++_Front];
+            _Count--;
+            if (_Front == _Rear)
+            {
+                _Front = -1;
+                _Rear = -1;
+            }
+            return value;
+        }
+
+        public int Size
+        {
+            get { return _Size; }
+        }
+
+        public int Count
+        {
+            get { return _Count; }
+        }
+
+        public T Peek()
+        {
+            if (this.IsEmpty())
+                throw new Exception("Очередь не заполнена.");
+
+            T value = _Array[_Front + 1];
+            return value;
+        }
+    }
+}
